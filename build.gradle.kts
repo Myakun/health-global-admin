7fc903ee211272721093966da06414a8a3798f45
Anatolii Zanosov <wegotmoves1799@gmail.com>
2026-03-14 15:39:41 +0100

    chore: configure build with Spring Boot and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,43 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "4.0.3"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.zanosov"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    implementation("org.flywaydb:flyway-core")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql")
+
+    implementation("com.fasterxml.uuid:java-uuid-generator:4.3.0")
+    implementation("me.paulschwarz:spring-dotenv:4.0.0")
+    implementation("org.jspecify:jspecify:1.0.0")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.test {
